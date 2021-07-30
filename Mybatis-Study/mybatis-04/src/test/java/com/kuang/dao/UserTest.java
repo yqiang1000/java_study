81347da2b7dcf39a5ad253e25f249dea4259e0b9
yqiang <yqiang1000@163.com>
2021-07-30 16:54:49 +0800

    add 日志

--- Mybatis-Study/mybatis-04/src/test/java/com/kuang/dao/UserTest.java
+++ Mybatis-Study/mybatis-04/src/test/java/com/kuang/dao/UserTest.java
@@ -4,29 +4,34 @@ import com.kuang.dao.UserMapper;
 import com.kuang.pojo.User;
 import com.kuang.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import java.util.List;
 
 public class UserTest {
-
+    // 配置日志
+    static Logger logger = Logger.getLogger(UserTest.class);
 
     @Test
     public void test() {
 
-        selectUserById();
-        selectAllUser();
+        selectUserById(1);
+//        selectAllUser();
 
     }
 
-    public void  selectUserById() {
+    public void  selectUserById(Integer id) {
+
+        logger.info("info:进入 selectUserById 方法， 参数 = " + id);
 
         SqlSession session = MybatisUtils.getSession();
         UserMapper mapper = session.getMapper(UserMapper.class);
-        User user = mapper.getOneUser(1);
+        User user = mapper.getOneUser(id);
         System.out.print(user);
 
         session.close();
+        logger.info("info:结束 selectUserById 方法， 参数 = " + id);
     }
 
     public void  selectAllUser() {
